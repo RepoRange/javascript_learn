8f5d10c213a296c7ac1efa3b900014219e3a85fe
Prasanna Patil <155186164+RepoRange@users.noreply.github.com>
2023-12-30 07:06:51 +0000

    datatypes and variables

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //treats all js code as newer version
+
+//alert(3 + 3) we are using node js not browser
+
+console.log(3 + 3)
+//no matter line spaces js prints next line itself
+
+
+
+console.log("suhas")
+
+
+//datatypes
+
+let name="suhas" //strings
+let age=18 //int
+let isLoggedIn=false  //boolean
+//null =>standalone value =>empty
+//undefined=>no value is assigned
+//symbol=>uniqueness
+
+console.log(typeof(age)) //output is number
+
+
+console.log(typeof(null)) //output is object
+
+
+console.log(typeof(undefined))  //output undefined
---
+++ 1_basics/variables.js
@@ -0,0 +1,23 @@
+const accountId=123456
+let accontEmail="suhas@googl.com"
+var accountPassword="27102004"
+accountCity="hospet"
+//in javaScript we can declare like this also but not recommended
+let accountState;
+ //here account state is unsefined 
+
+
+
+
+// accountId=25633   is not allowed its constant
+
+
+// prefer not use var,
+// because of issue in block and functional scope 
+
+accontEmail="suhasjavali@googl.com"
+ accountPassword="101010"
+accountCity="koppal"
+
+console.table([accontEmail,accountId,accountPassword,accountCity,accountState]);
+
