331e319838338543137691e1bf7080bd28326e24
Prasanna Patil <155186164+RepoRange@users.noreply.github.com>
2023-12-30 12:06:54 +0000

    conversions and operations

---
+++ 1_basics/03_conversionsOperation.js
@@ -0,0 +1,47 @@
+ let score=33
+// console.log (typeof(score))//output is number
+
+//type csating
+ let score1="45abs" //this is string 
+ let typecast=Number(score1) //typecasting is done 
+  //console.log(typeof(typecast))  //printing it
+ //console.log(typecast)//output is NaN => not an number
+
+//null=>typecast to Number=>output is 0
+//undefined=>typecast to number=>output is NaN
+//boolean=>typecast to number=>out put is 1 or 0
+//string=>NaN
+
+let x1=12454
+let x2=Boolean(x1)
+ //console.log(x2)
+// number>0=>output is true
+//""=>false
+//"suhas"=>trur
+
+
+//*******************************Operations*********************************/
+
+
+let str1="hello"
+let str2=" suhas"
+// console.log(str1 + str2)
+
+
+// console.log("1"+ 2)//=>12
+// console.log(1+ "2")//=>12
+// console.log(1+ 2+"3")//=>33
+// console.log("1"+ 2+3)//=>123
+//string is first then all are treated as string
+//if string is last initial operation is done and merged
+
+
+// console.log(+true)//=>1 incrementing true is 1 but (true+)=>error
+// console.log(+"")//=>0=>false
+
+
+let game=34
+++game //or game++ is also allowed (prefix and postfix operator)
+console.log(game)//to learn use mdn
+
+
